c54f6be3d31cd2e9a0869307986915c487b65879
Piramida256 <163049197+Piramida256@users.noreply.github.com>
2026-05-03 00:05:26 +0300

    Update VENUS_OUT_DIR and create directory
    Change VENUS_OUT_DIR to use /dev/shm for temporary files and ensure the directory is created before executing the server.

--- venus.sh
+++ venus.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 export VENUS_PROVER_GRPC_PORT=7000
 export VENUS_DIR="/root/venus_v0_1_6"
-export VENUS_OUT_DIR="$VENUS_DIR/tmp"
+export VENUS_OUT_DIR="/dev/shm/venus_tmp"
 export RUST_LOG=info
-~/cysic-prover/venus_prover_server
 
+mkdir -p "$VENUS_OUT_DIR"
+exec ~/cysic-prover/venus_prover_server

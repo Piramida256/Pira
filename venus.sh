e88b1ffa61aa1a57d651b9311651d14ed59d5977
Piramida256 <163049197+Piramida256@users.noreply.github.com>
2026-05-06 00:23:44 +0300

    Old on ssd

--- venus.sh
+++ venus.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 export VENUS_PROVER_GRPC_PORT=7000
 export VENUS_DIR="/root/venus_v0_1_6"
-export VENUS_OUT_DIR="/dev/shm/venus_tmp"
+export VENUS_OUT_DIR="$VENUS_DIR/tmp"
 export RUST_LOG=info
-
-mkdir -p "$VENUS_OUT_DIR"
-exec ~/cysic-prover/venus_prover_server
+~/cysic-prover/venus_prover_server
